7775eb9a5c87cb5e59a2137d9fce8d2d1cbff507
jackcolin21307 <jackcolin21307@gmail.com>
2022-04-23 15:54:30 -0700

    Moved Firebase setup in single file

--- src/database.js
+++ src/database.js
@@ -1,22 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import app from "./firebaseSetup.js"
 import { getFirestore , collection, addDoc } from "firebase/firestore"; 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBI-bf_fmT7aNBKE5wRfFodDA_2gs0nvys",
-  authDomain: "florafauna-d7d60.firebaseapp.com",
-  projectId: "florafauna-d7d60",
-  storageBucket: "florafauna-d7d60.appspot.com",
-  messagingSenderId: "912979841637",
-  appId: "1:912979841637:web:2b4c0430f5dbfaeb576c8c"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 async function addPinpoint(x, y){
---
+++ src/firebaseSetup.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBI-bf_fmT7aNBKE5wRfFodDA_2gs0nvys",
+  authDomain: "florafauna-d7d60.firebaseapp.com",
+  projectId: "florafauna-d7d60",
+  storageBucket: "florafauna-d7d60.appspot.com",
+  messagingSenderId: "912979841637",
+  appId: "1:912979841637:web:2b4c0430f5dbfaeb576c8c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;

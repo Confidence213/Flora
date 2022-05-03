4e338dcb39ed365470897e28d1e253e4dbd41b56
jackcolin21307 <jackcolin21307@gmail.com>
2022-05-02 21:59:55 -0700

    Can Upload User Posts
    Added functionality for uploading user posts through the function addNewPost(). This function takes in a Post object as its only argument. Removed addPinpoint and uploadImage prototype functions.

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -1,28 +1,60 @@
-// Import the functions you need from the SDKs you need
 import app from "./firebaseSetup.js"
-import { getFirestore , collection, addDoc } from "firebase/firestore"; 
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getFirestore , collection, setDoc, doc } from "firebase/firestore"; 
+import { getStorage, ref, uploadBytes, getDownloadURL  } from "firebase/storage";
 
 const storage = getStorage(app);
 const db = getFirestore(app);
 
-function uploadImage(file){
-    let name = 'images/' + file.name;
-    const storageRef = ref(storage, name);
-    uploadBytes(storageRef, file).then((snapshot) => {
-        console.log('Uploaded a blob or file!');
-    });
-}
-
-async function addPinpoint(x, y){
-    try {
-        const test = await addDoc(collection(db, "posts"), {
-            xCoordinate: x,
-            yCoordinate: y,
-        });
-        console.log("Document written with ID: ", test.id);
-    } catch (e) {
-        console.error("Error adding document: ", e);
+//NOTE: image field should be a File or Blob object when inserting data into database, but should be a URL when getting data.
+class Post{
+    constructor(author, title, description, species, image, latitude, longitude, rating=0){
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.species = species;
+        this.image = image;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.rating = rating;
     }
 }
-export {addPinpoint, uploadImage};
+
+const postConverter = {
+    toFirestore: (post) => {
+        return {
+                author: post.author,
+                title: post.title,
+                description: post.description,
+                species: post.species,
+                image: post.image,
+                latitude: post.latitude,
+                longitude: post.longitude,
+                rating: post.rating,
+            };
+    },
+    fromFirestore: (snapshot, options) => {
+        const data = snapshot.data(options);
+        return new Post(data.author, data.title, data.description, data.species, data.image, data.latitude, data.longitude, data.rating);
+    }
+};
+
+async function addNewPost(post){
+    const newPostRef = doc(collection(db, "posts")).withConverter(postConverter);
+
+    let imagePath = 'images/' + newPostRef.id;
+    const storageRef = ref(storage, imagePath);
+    await uploadBytes(storageRef, post.image).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+    });
+
+    let imageURL = "";
+    await getDownloadURL(ref(storage, imagePath))
+    .then((url) => {
+      imageURL = url;
+    });
+    
+    post.image = imageURL;
+    setDoc(newPostRef,post);
+}
+
+export {Post, addNewPost};

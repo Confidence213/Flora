6e4a6c48f4045c86e3ac24ea640daed00095ba5a
jackcolin21307 <jackcolin21307@gmail.com>
2022-05-05 13:50:37 -0700

    Incomplete login page (still need sign up)

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ function App() {
     <div className="app">
       <div className="login-form">
         <div className="title">Sign In</div>
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+        {isSubmitted ? <div>User successfully logged in</div> : renderForm}
       </div>
     </div>
   );

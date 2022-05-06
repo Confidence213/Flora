3f0ea38893c6d080b6847fdbe688001742ad6a9b
jackcolin21307 <jackcolin21307@gmail.com>
2022-05-05 22:50:00 -0700

    Add leaflet to node modules, and put a basic map on the page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header'
+import Map from './pages/Map/Map'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Map />
     </div>
   );
 }

af55df4dcb5429c12003ccb669cf0b2fe9c2a8b0
jackcolin21307 <jackcolin21307@gmail.com>
2022-05-06 23:23:18 -0700

    Login and signup are now columns with separate inputs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,108 @@
 import './App.css';
 import React, { useState } from "react";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {lusername: '', lpassword: '', susername: '', 
+    spassword: '', semail: '', isSubmitted: false};
+
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleSignUpSubmit = this.handleSignUpSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name
+    this.setState({[name]: event.target.value});
+  }
+
+  handleLoginSubmit(event) {
+    this.setState({isSubmitted: true});
+    alert('A name was submitted: ' + this.state.lusername + " and a password: " + this.state.lpassword);
+    event.preventDefault();
+  }
+
+  handleSignUpSubmit(event) {
+    this.setState({isSubmitted: true});
+    alert('A name was submitted: ' + this.state.susername + " and a password: " + this.state.spassword
+    + "email: " + this.state.semail);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <login-table><tr>
+        <div className="app">
+          <div className="login-form" >
+            <td>
+              <div className="title" style={{textAlign: "center"}}>Login</div>
+              {this.state.isSubmitted ? <div>User successfully logged in</div> : 
+              <div className="form">
+              <form onSubmit={this.handleLoginSubmit}>
+                <div className="input-container">
+                  <label>
+                    Username:
+                  </label>
+                  <input name="lusername" type="text" value={this.state.lusername} onChange={this.handleChange} required/>
+                </div>
+                <div className="input-container">
+                  <label>
+                    Password:
+                  </label>
+                  <input name="lpassword" type="text" value={this.state.lpassword} onChange={this.handleChange} required/>
+                </div>
+                <div className="button-container">
+                  <input type="submit" />
+                </div>
+              </form>
+            </div>
+              }
+            </td>
+            <td>
+              <div className="title" style={{textAlign: "center"}}>SignUp</div>
+              {this.state.isSubmitted ? <div>User successfully signed up</div> : 
+              <div className="form">
+              <form onSubmit={this.handleSignUpSubmit}>
+                <div className="input-container">
+                  <label>
+                    Email:
+                  </label>
+                  <input name="semail" type="text" value={this.state.semail} onChange={this.handleChange} required/>
+                </div>
+                <div className="input-container">
+                  <label>
+                    Username:
+                  </label>
+                  <input name="susername" type="text" value={this.state.susername} onChange={this.handleChange} required/>
+                </div>
+                <div className="input-container">
+                  <label>
+                    Password:
+                  </label>
+                  <input name="spassword" type="text" value={this.state.spassword} onChange={this.handleChange} required/>
+                </div>
+                <div className="button-container">
+                  <input type="submit" />
+                </div>
+              </form>
+            </div>
+              }
+            </td>
+          </div>
+        </div>
+      </tr></login-table>
+    );
+  }
+}
+
+/*function App() {
   // React States
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  // Dummy User Login info
+  // Dummy User Login database
   const database = [
     {
       username: "user1",
@@ -102,17 +198,22 @@ function App() {
   );
 
   return (
-    <div className="app">
-      <div className="login-form">
-      <h2>FloraFauna</h2>
-        <div className="title">Login</div>
-        {isSubmitted ? <div>User successfully logged in</div> : renderLoginForm}
-        <div className="title">Sign Up</div>
-        {isSubmitted ? <div>User successfully signed up</div> : renderSignUpForm}
+    <login-table><tr>
+      <div className="app">
+        <div className="login-form" >
+          <td>
+            <div className="title" style={{textAlign: "center"}}>Login</div>
+            {isSubmitted ? <div>User successfully logged in</div> : renderLoginForm}
+          </td>
+          <td>
+            <div className="title" style={{textAlign: "center"}}>Sign Up</div>
+            {isSubmitted ? <div>User successfully signed up</div> : renderSignUpForm}
+          </td>
+        </div>
       </div>
-    </div>
+    </tr></login-table>
   );
-}
+}*/
 
 
 

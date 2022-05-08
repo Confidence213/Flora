10b4e9fb281f9be933b7543930a84429dda359ef
jackcolin21307 <jackcolin21307@gmail.com>
2022-05-08 14:03:06 -0700

    home page not yet linked to map

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,144 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Home.css';
+import './Home.css'
 
+function uploadPage() {
 
-/*This class is equivalent to the following function:
-class Square extends React.Component {
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
+  let speciesInput = React.createRef();
+  let milesInput = React.createRef();
+  let locationInput = React.createRef();
+  
+  const handleClick = (a) => {
+    console.log(speciesInput.current.value);
+    console.log(milesInput.current.value);
+    console.log(locationInput.current.value);
   }
-}*/
 
-function Square(props) {
   return (
-      <button 
-        className="square" 
-        onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
+    <div className="page">
+      <table className="inputs">
+        <tr><h3>Find Community Sightings:</h3></tr>
+        <tr id="LOCATIONinput">
+          <input id="locinput" ref={locationInput} placeholder="Search Location..." />
+          <input id="minput" ref={milesInput} placeholder="miles..." />
+        </tr>
+        
+        <tr><input id="spinput" ref={speciesInput} placeholder="Search Species..." /></tr>
+        <tr><button onClick={handleClick} className="search">SEARCH</button></tr>
+      </table>
+      <img id="map" src="https://svgshare.com/i/gvs.svg" alt="graphic_map"/>
+    </div>
+  );
 }
 
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true, //default start as X
-    };
-  }
-
-  handleClick(i) { //is called when props.onClick in square
-    const squares = this.state.squares.slice();
-    //squares[i] = this.state.xIsNext ? 'X' : 'O';
-
-    if (calculateWinner(squares) || squares[i]) { //do nothing if someone won
-      return;
-    }
-
-    if (this.state.xIsNext)
-    {
-      squares[i] = 'X';
-    } else
-    {
-      squares[i] = 'O';
-    }
-
-    this.setState({
-      squares: squares, 
-      xIsNext: !this.state.xIsNext, //flips xIsNext
-    });
-  }
-
-  renderSquare(i) {
-    return (
-      <Square 
-        value={this.state.squares[i]} 
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
-  render() {
-
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Home PlaceHolder: Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-    //const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-export default Game;
+export default uploadPage;

ab3aa2342b1dbb5d9fdbcc1be1488219703e6c04
jackcolin21307 <jackcolin21307@gmail.com>
2022-05-11 16:54:36 -0700

    setup account functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import Header from './components/Header/Header'
 import Map from './pages/Map/Map'
+import {makeUser, signIn, signOutUser, getUsername} from './firebase/account'
 
 function App() {
+  let foo = signIn("foo@example.com", "wrong");
+  console.log(foo)
   return (
     <div className="App">
       <Header />

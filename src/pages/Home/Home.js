89608eaf1d7452b7c0d19dcaa95d0436c3ea9b19
jackcolin21307 <jackcolin21307@gmail.com>
2022-05-25 10:54:14 -0700

    home working w/ more images

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,34 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { setSelectionRange } from '@testing-library/user-event/dist/utils';
+import React, {useState} from 'react';
 import './Home.css'
 
-function uploadPage() {
+function UploadPage() {
+  const [showLoc, setShowLoc] = useState(false);
 
   let speciesInput = React.createRef();
-  let milesInput = React.createRef();
-  let locationInput = React.createRef();
-  
+  let longInput = React.createRef();
+  let latInput = React.createRef();
+
   const handleClick = (a) => {
-    console.log(speciesInput.current.value);
-    console.log(milesInput.current.value);
-    console.log(locationInput.current.value);
+    if (!(longInput.current.value == '' || latInput.current.value == '' || isNaN(longInput.current.value) || isNaN(latInput.current.value)))
+      window.location = "/map/" + latInput.current.value + "/" + longInput.current.value + "/9/" + speciesInput.current.value;
+    else
+      setShowLoc(true);
+  }
+
+  const handleLocation = (a) => {
+    if (!"geolocation" in navigator) {
+      alert("error: geological data not available")
+    }
+    else {
+      navigator.geolocation.getCurrentPosition( (position) => {    
+        latInput.current.value = position.coords.latitude;
+        longInput.current.value = position.coords.longitude;
+      });
+    }
   }
 
   return (
-    <div className="page">
-      <table className="inputs">
-        <tr><h3>Find Community Sightings:</h3></tr>
-        <tr id="LOCATIONinput">
-          <input id="locinput" ref={locationInput} placeholder="Search Location..." />
-          <input id="minput" ref={milesInput} placeholder="miles..." />
-        </tr>
-        
-        <tr><input id="spinput" ref={speciesInput} placeholder="Search Species..." /></tr>
-        <tr><button onClick={handleClick} className="search">SEARCH</button></tr>
-      </table>
-      <img id="map" src="https://svgshare.com/i/gvs.svg" alt="graphic_map"/>
+    <div>
+      <div className="page">
+        <table className="inputs">
+          <tr><h3>Find Community Sightings:</h3></tr>
+          <tr id="LOCATIONinput">
+            <input ref={latInput} id="lat" placeholder="*Enter Latitude..." />
+            <input ref={longInput} id="long" placeholder="*Enter Longitude..." />
+            {showLoc && <p id='speciesError'>Please enter valid lat and long</p>}
+          </tr>
+          
+          <tr><input id="spinput" ref={speciesInput} placeholder="Search Species...(optional)" /></tr>
+          <tr><button onClick={handleLocation} className="post">Get Current Location</button>
+          <button onClick={handleClick} className="search">SEARCH</button></tr>
+        </table>
+        <img id="map" src="https://svgshare.com/i/gvs.svg" alt="graphic_map"/>
+      </div>
+      <h2></h2>
+        <h3 id="title"> This Week's Gallery</h3> <h2></h2>
+        <img class="img" src="https://media.thetab.com/blogs.dir/179/files/2017/04/matts-pic-600x284.jpg"/>
+        <img class="img" src="https://i.imgur.com/dligDo7.jpg"/>
+        <img class="img" src="https://i.imgur.com/GLpoJXA.jpg"/>
+        <img class="img" src="https://i.redd.it/oxqwjebtg5g81.jpg"/>
+        <img class="img" src="https://i.redd.it/f4vi3qlv24t71.jpg"/>
+      
     </div>
   );
 }
 
-export default uploadPage;
+export default UploadPage;
